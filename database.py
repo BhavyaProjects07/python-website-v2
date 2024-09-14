c6d47435e7dbd857c0f35e7122764db42c5a001b
BhavyaProjects07 <laptopuse01824x@gmail.com>
2024-09-14 20:01:05 +0530

    added connection to my db

--- database.py
+++ database.py
@@ -1,1 +1,23 @@
-import sqlalchemy
+import pymysql
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+# Load environment variables
+env_path = Path('templates') / 'info.env'
+load_dotenv(dotenv_path=env_path)
+
+def get_db_connection():
+    timeout = 15
+    return pymysql.connect(
+        charset=os.getenv('DB_CHARSET'),
+        connect_timeout=timeout,
+        cursorclass=pymysql.cursors.DictCursor,
+        database=os.getenv('DB_NAME'),
+        host=os.getenv('DB_HOST'),
+        password=os.getenv('DB_PASSWORD'),
+        read_timeout=timeout,
+        port=int(os.getenv('DB_PORT')),
+        user=os.getenv('DB_USER'),
+        write_timeout=timeout,
+    )
--- flsk.py
+++ flsk.py
@@ -1,50 +1,41 @@
 from flask import Flask,render_template
 from flask import jsonify
+from database import get_db_connection
 import os
 app = Flask(__name__,static_folder='static')
 Picfolder = os.path.join('static','images')
 app.config['UPLOAD_FOLDER'] = Picfolder
-JOBS = [
 
-    
-    {
-        'Id': 1,
-        'Title' : 'Data Anaylist',
-        'Location' : 'Philedelphia , USA',
-        'Salary' : '$1,20,000'
-    },
-    {
-        'Id': 2,
-        'Title' : 'Web Devloper',
-        'Location' : 'California , USA',
-        'Salary' : '$1,00,000'
-    },
-    {
-        'Id': 3,
-        'Title' : 'App Devloper',
-        'Location' : 'Bengaluru , India',
-        'Salary' : '$1,50,000'
-    },
-    {
-        'Id': 4,
-        'Title' : 'FrontEnd Engineer',
-        'Location' : 'Dallas , Texas',
-        'Salary' : '$84599'
-    }
-]
+
+
+
+def load_jobs_from_db():
+    jobs = []
+    connection = get_db_connection()  # Open a new connection
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM jobs")
+            result = cursor.fetchall()
+            for i in result:
+                jobs.append(i)
+    finally:
+        connection.close()  # Close the connection after the query
+    return jobs
+
 
 
 @app.route('/')
 def hello_world():
+    jobs = load_jobs_from_db()
     pic1 = os.path.join(app.config['UPLOAD_FOLDER'],'football.jpeg')
     pic2 = os.path.join(app.config['UPLOAD_FOLDER'],'logo.svg')
     pic3 = os.path.join(app.config['UPLOAD_FOLDER'],'people.jpeg')
-    return render_template('web.html',jobs = JOBS,company_name='ALL',user_image = pic1,user_image2 = pic2,user_image3 = pic3)
+    return render_template('web.html',jobs = jobs,company_name='ALL',user_image = pic1,user_image2 = pic2,user_image3 = pic3)
 
 
 @app.route("/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    return jsonify(list_jobs)
 
 if __name__ == '__main__':
     app.run(debug=True) 
